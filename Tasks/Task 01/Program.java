d357667af5c1a10a881ea6016c57b69658376f4d
Your Name <you@example.com>
2019-01-05 20:29:58 +0300

    domashka

---
+++ Tasks/Task 01/Program.java
@@ -0,0 +1,54 @@
+class program  {
+	public static void main (String[] args){
+		int sum=15867;
+		int money1=5000;
+		int money2=2000;
+		int money3=1000;
+		int money4=500;
+		int money5=100;
+		int money6=50;
+		int money7=10;
+		int money8=5; 
+		int money9=1;
+
+			int x1 = sum / money1;  
+			int y1 = sum % money1;
+			System.out.println ("5000 - " + x1);
+
+			int x2 = sum / money2;  
+			int y2 = sum % money2;
+			System.out.println ("2000 - " + x2);
+
+			int x3 = sum / money3;  
+			int y3 = sum % money3;
+			System.out.println ("1000 - " + x3);
+
+			int x4 = sum / money4;  
+			int y4 = sum % money4;
+			System.out.println ("500 - " + x4);
+
+			int x5 = sum / money5;  
+			int y5 = sum % money5;
+			System.out.println ("100 - " + x5);
+
+			int x6 = sum / money6;  
+			int y6 = sum % money6;
+			System.out.println ("50 - " + x6);
+
+			int x7 = sum / money7;  
+			int y7 = sum % money7;
+			System.out.println ("10 - " + x7);
+
+
+			int x8 = sum / money8;  
+			int y8 = sum % money8;
+			System.out.println ("5 - " + x8);	
+
+
+			int x9 = sum / money9;  
+			int y9 = sum % money9;
+			System.out.println ("1 - " + x9);	
+
+	}
+	
+}
---
+++ Tasks/Task 02/binary.java
@@ -0,0 +1,47 @@
+class Program2 { 
+
+ 		public static void main(String[] args) { 
+
+ 		int numb = 137; 
+
+ 		int a = 2; 
+
+ 		int b = numb / a; 
+
+ 		int c = numb % a; 
+
+ 		int d = b / a; 
+
+ 		int e = b % a; 
+
+ 		int f = d / a; 
+
+ 		int g = d % a; 
+
+ 		int h = f / a; 
+
+ 		int i = f % a; 
+
+ 		int j = h / a; 
+
+ 		int k = h % a; 
+
+ 		int l = j / a; 
+
+ 		int m = j % a; 
+
+ 		int n = l / a; 
+
+ 		int o = l % a; 
+
+ 		int p = n / a; 
+
+ 		int q = n % a; 
+
+ 
+
+ System.out.println(q + " " + o + " " + m + " " + k + " " + i + " " + g + " " + e + " " + c + " "); 
+
+ } 
+
+}
